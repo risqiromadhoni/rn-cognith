a2ad8194268e1e957aaa6e804fe400dc7feed576
Risqi Romadhoni <risqi.romadhoni96@gmail.com>
2024-06-24 11:06:31 +0700

    chore: update dependencies and add testing library for React Native

--- src/components/__tests__/ThemedText-test.tsx
+++ src/components/__tests__/ThemedText-test.tsx
@@ -1,12 +1,10 @@
+import { render } from "@testing-library/react-native";
 import * as React from "react";
-import renderer from "react-test-renderer";
 
 import { ThemedText } from "../ThemedText";
 
 it("renders correctly", () => {
-	const tree = renderer
-		.create(<ThemedText>Snapshot test!</ThemedText>)
-		.toJSON();
+	const { toJSON: tree } = render(<ThemedText>Snapshot test!</ThemedText>);
 
-	expect(tree).toMatchSnapshot();
+	expect(tree()).toMatchSnapshot();
 });
